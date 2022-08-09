8e3794399c5a4bbcad8620e7518bdf5ce87fdd4a
PeytonCast <107663364+PeytonCast@users.noreply.github.com>
2022-08-09 12:08:44 -0400

    Update weather-script.js
    In this commit, I add the start to my search history.  I have it set up to create an array in local storage and add to that array every time I search for something. I'd like to say I have a plan on how I'm going to validate the onclick events but honestly, I have no clue how I'm going to do that.
    Inch by inch life's a cinch.

--- develop/weather-script.js
+++ develop/weather-script.js
@@ -1,10 +1,7 @@
 
-var search = $('#search')
-var form = $('#form')
-//var recentSearched =$('#recent-search1')
 var weather = 
 {
-APIKey: '32400c2b40785d6c9c099ed3c9d49d48',
+APIKey: 'b7c41b3ca52d37ef2ccc92658a1d9029',
 fetchWeatherData: function (city) {
 fetch( "https://api.openweathermap.org/data/2.5/weather?q="
 + city 
@@ -20,31 +17,69 @@ var { name } = data;
 var { icon, description } = data.weather[0];
 var { temp, humidity } = data.main;
 var { speed } = data.wind;
-var { feels_like } = data.main
-console.log( 'name',name, 'icon',icon, 'description',description, 'temp',temp, 'humid',humidity, 'wind speed',speed)
+//var { uvi } = data.uvi
+//displays the data from the api as an HTML element
 document.getElementById('city-name').innerText = name;
 document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + ".png"
 document.getElementById('discription').innerText = description;
 document.getElementById('Temp').innerText = 'Current temp is ' + temp + 'F°';
 document.getElementById('wind').innerText = 'Wind speed ' + speed + ' mph';
-document.getElementById('feels').innerText = 'Feels like ' + feels_like + 'F°';
+//document.getElementById('feels').innerText = ' ' + uvi + '';
 document.getElementById('humidity').innerText = 'Humidity: ' + humidity + '%';
 
 },
+//valadates the content of the search box input
+
 SearchResults: function() {
     this.fetchWeatherData(document.querySelector(".search-box").value);
+    //get dat from search to add to search history
+    var new_data = document.querySelector(".search-box").value
+    //creates an empty array if there is no data
+    if (localStorage.getItem('data')== null) {
+        localStorage.setItem('data', '[]')
+    }
+    //adds old data to new data making a bigger array which is a search history
+    var old_data = JSON.parse(localStorage.getItem('data'))
+    old_data.push(new_data)
+    //save change
+    localStorage.setItem('data', JSON.stringify(old_data));
+
+    runHistory()
+    
 },
 };
+// button to run the functions above
 document.querySelector(".search button").addEventListener('click', function() {
     weather.SearchResults()
 })
 
-   
 
-//city.on('input', searchWeather=>{cityName.text(searchWeather.target.value) })
-//var getData = () => {localStorage.setItem('cityWeather', cityName.text())}
-//search.on('click', getData)
+    var empty = []
 
+    //i need to create a search history fully iteracable
+    //IF i input content in my search box (event)
+    var inputBox = document.getElementById('search')
+    var searchHistory = document.getElementById('search-history')
+    var getlocal = localStorage.getItem('history')
+    let last = empty[empty.length - 1]
+  
+  function runHistory() {
+    if (inputBox = String){
+       
+        var button = document.createElement ('p')
+        button.setAttribute('id', localStorage.getItem('setItem'))
+        button.setAttribute('onclick', 'historyAutoFill()')
+        button.textContent = localStorage.getItem('history' )
+        searchHistory.appendChild(button)
+        localStorage.setItem('storedBtn', button)
+        
+    }
+}
+ 
 
-//cityName.text(localStorage.getItem('cityWeather'))
-//recentSearched.appendChild(localStorage.getItem('cityWeather'))
+function historyAutoFill()
+{
+    
+    weather.fetchWeatherData()
+}
+
